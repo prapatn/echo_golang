3e1cb0a760bd0b8a5c6969ab1ffc692b43880636
prapatn <king_44peeraphat@hotmail.com>
2023-06-07 10:47:39 +0700

    edit repo

--- repository/customer_repo.go
+++ repository/customer_repo.go
@@ -2,43 +2,37 @@ package repository
 
 import (
 	"echo_golang/database"
+	"echo_golang/model"
 )
 
-type CustomerRepo interface {
-	GetByID(customer interface{}, id interface{}) error
-	GetAll(customers interface{}) error
-	Insert(customer interface{}) error
-	Update(customer interface{}) error
-	Delete(customer interface{}, id interface{}) error
+// type CustomerUsecase struct{
+// 	CustomerRepo
+// }
+
+// type CustomerRepo interface {
+// 	GetByID(customer interface{}, id interface{}) error
+// 	GetAll(customers interface{}) error
+// 	Insert(customer interface{}) error
+// 	Update(customer interface{}) error
+// 	Delete(customer interface{}, id interface{}) error
+// }
+
+func GetByID(customer *model.Customer, id interface{}) error {
+	return database.DB.Where("id = ?", id).First(customer).Error
 }
 
-func GetByID(customer interface{}, id interface{}) error {
-	db := database.GetDBInstance()
-	err := db.Where("id = ?", id).First(customer).Error
-	return err
+func GetAll(customers *[]model.Customer) error {
+	return database.DB.Find(customers).Error
 }
 
-func GetAll(customers interface{}) error {
-	db := database.GetDBInstance()
-	err := db.Find(customers).Error
-	return err
+func Insert(customer *model.Customer) error {
+	return database.DB.Create(customer).Error
 }
 
-func Insert(customer interface{}) error {
-	db := database.GetDBInstance()
-	err := db.Create(customer).Error
-	return err
+func Update(customer *model.Customer) int64 {
+	return database.DB.Model(customer).Updates(customer).RowsAffected
 }
 
-func Update(customer interface{}) int64 {
-	db := database.GetDBInstance()
-	rowAffected := db.Model(customer).Updates(customer).RowsAffected
-
-	return rowAffected
-}
-
-func Delete(customer interface{}, id interface{}) int64 {
-	db := database.GetDBInstance()
-	rowAffected := db.Where("id = ?", id).Delete(customer).RowsAffected
-	return rowAffected
+func Delete(customer *model.Customer, id interface{}) int64 {
+	return database.DB.Where("id = ?", id).Delete(customer).RowsAffected
 }
